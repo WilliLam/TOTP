f3e5f154352c6038f1f0a2e6c5c397cc803cd96c
william <la.william4@gmail.com>
2020-08-27 13:54:39 +0100

    added tests and ensured digits length is always correct

--- totp/main.py
+++ totp/main.py
@@ -2,8 +2,7 @@ import hashlib
 import hmac
 import time
 
-
-class TOTP:
+class HOTP:
     def __init__(self, digits=6, digestmod="sha1"):
         """
 
@@ -21,33 +20,24 @@ class TOTP:
         Offset = digest[-1] & 0x0f
         P = ((digest[Offset] & 0x7f) << 24 | (digest[Offset + 1] & 0xff) << 16 | (digest[Offset + 2] & 0xff) << 8 | \
              (digest[Offset + 3] & 0xff))
-        return P % (10 ** digits)
+        P =  str(P % (10 ** digits))
+        if len(P) < digits:
+            P = "0" + P
+        return P
 
     def hotp(self, k, c=-1):
         if type(c) == str:
             c = bytes.fromhex(c)
         elif c == -1:
             c = self.counter.to_bytes(8, byteorder="big")
+            self.counter += 1
         # hmac.digest(k, msg=c, digest="sha512")
         # print(self, c)
         nhmac = hmac.new(key=k, msg=c, digestmod=self.digestmod)
         # print("h2", (hashlib.sha3_512(self).digest()), "size", hashlib.sha3_512(self).digest_size, )
         return self.truncate(nhmac.digest(), self.digits)
 
-    def totp(self, k, window=30):
-        """
-        based on https://tools.ietf.org/html/rfc6238#page-4
-        secret string : key to generate password from
-        digits int : length of password to generate
-        window int : how long before step(c) increments
-        """
-        # c is counter in hotp - check https://tools.ietf.org/html/rfc4226#page-5
-        c = hex(int(time.time() // window))[2:]
-        while len(c) < 16:
-            c = "0" + c
-        return self.hotp(k, c)
-
-    def verify(self, k, code, window=30, allowed_steps=2):
+    def verify(self, k, code, counter = -1, window=30, allowed_steps=2):
         """
         checks if the code is valid, max time allowed being window*allowed_steps
         :param k: secret
@@ -56,10 +46,76 @@ class TOTP:
         :param allowed_steps: n steps before to match
         :return: boolean depending on verify successful
         """
+        # if counter == -1:
+        if code == self.hotp(k, counter):
+            return True
+        return False
+
+class TOTP(HOTP):
+    def __init__(self, digits=6, digestmod="sha1"):
+        """
+
+        :param key:  shared key/keygen
+        :param msg:  counter
+        :param digestmod: check hashlib.algorithms_guaranteed or hashlib.algorithms_available -
+        defaults to the hmac sha-1 implementation
+        """
+        # self.key = key
+        super().__init__(digits, digestmod)
+        self.digits = digits
+        self.digestmod = digestmod
+    #
+    # def truncate(self, digest, digits):
+    #     Offset = digest[-1] & 0x0f
+    #     P = ((digest[Offset] & 0x7f) << 24 | (digest[Offset + 1] & 0xff) << 16 | (digest[Offset + 2] & 0xff) << 8 | \
+    #          (digest[Offset + 3] & 0xff))
+    #     return P % (10 ** digits)
+
+    # def hotp(self, k, c=-1):
+    #     if type(c) == str:
+    #         c = bytes.fromhex(c)
+    #     elif c == -1:
+    #         c = self.counter.to_bytes(8, byteorder="big")
+    #     # hmac.digest(k, msg=c, digest="sha512")
+    #     # print(self, c)
+    #     nhmac = hmac.new(key=k, msg=c, digestmod=self.digestmod)
+    #     # print("h2", (hashlib.sha3_512(self).digest()), "size", hashlib.sha3_512(self).digest_size, )
+    #     return self.truncate(nhmac.digest(), self.digits)
+
+    def totp(self, k, c = -1, window=30):
+        """
+        based on https://tools.ietf.org/html/rfc6238#page-4
+        secret string : key to generate password from
+        digits int : length of password to generate
+        window int : how long before step(c) increments
+        """
+        # c is counter in hotp - check https://tools.ietf.org/html/rfc4226#page-5
+        if c == -1:
+            c = hex(int(time.time() // window))[2:]
+            while len(c) < 16:
+                c = "0" + c
+            return self.hotp(k, c)
+        else:
+            return self.hotp(k, c)
+
+    def verify(self, k, code, counter = -1, window=30, allowed_steps=2):
+        """
+        checks if the code is valid, max time allowed being window*allowed_steps
+        :param k: secret
+        :param code: number from request totp
+        :param window: n seconds for each step
+        :param allowed_steps: n steps before to match
+        :return: boolean depending on verify successful
+        """
+        # if counter == -1:
+        #     verifycode = self.hotp(k, counter)
+        # else:
         for i in range(0, allowed_steps + 1):
             c = hex(int((time.time() - i * window) // window))[2:]
             while len(c) < 16:
                 c = "0" + c
-            if code == self.hotp(k, c):
+
+            verifycode = self.totp(k, c, window=window)
+            if code == verifycode:
                 return True
         return False
--- totp/test/test_main.py
+++ totp/test/test_main.py
@@ -2,13 +2,17 @@ from totp.main import TOTP
 from time import sleep
 
 def test_len_totp():
+    test1 = TOTP(digits=6, digestmod="sha1")
+    assert len(str(test1.totp(b"whatever", window=30))) == 6
     test1 = TOTP(digits=6, digestmod="sha256")
     assert len(str(test1.totp(b"whatever", window=30))) == 6
-
+    test1 = TOTP(digits=10, digestmod="sha512")
+    assert len(str(test1.totp(b"whatever", window=30))) == 10
 
 def test_verify_totp():
     test1 = TOTP(digits=6, digestmod="sha256")
     passcode = test1.totp(b"213124213123123123231232", window = 3)
-    sleep(5)
+    sleep(1)
     assert test1.verify(b"213124213123123123231232", passcode,window=3, allowed_steps=3)
-
+    sleep(6)
+    assert test1.verify(b"213124213123123123231232", passcode, window=3, allowed_steps=1) == False
